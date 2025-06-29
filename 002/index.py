58740663f9e96fef21366484384d0c4f69a2f6b7
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2025-06-29 19:32:42 -0300

    Adiciona questão 002

---
+++ 002/index.py
@@ -0,0 +1,10 @@
+def soma_numeros():
+  numero1 = 12
+  numero2 = 14
+  soma = numero1 + numero2
+  
+  print(f"Número 1: {numero1}")
+  print(f"Número 2: {numero2}")
+  print(f"Soma: {soma}")
+
+soma_numeros()
