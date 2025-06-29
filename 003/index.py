eb2254ad0825de328f695a9f86fcb16d70f1f458
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2025-06-29 19:32:46 -0300

    Adiciona questão 003

---
+++ 003/index.py
@@ -0,0 +1,14 @@
+def volume_caixa():
+  comprimento = 12
+  largura = 14
+  altura = 20
+  
+  volume = comprimento * largura * altura
+  
+  print(f"Dimensões da caixa:")
+  print(f"Comprimento: {comprimento} cm")
+  print(f"Largura: {largura} cm")
+  print(f"Altura: {altura} cm")
+  print(f"Volume: {volume} cm³")
+
+volume_caixa()
